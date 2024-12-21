1254e7079b01a6a7a7abbdf659b8779f1e957dbe
Sarthak Singh <mzppsarthakjvb@gmail.com>
2024-12-22 00:16:12 +0530

    Portfolio started

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+
+import "./app.scss";
+import Cursor from './components/cursor/Cursor'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/hero/Hero'
+import Parallax from './components/parallax/Parallax'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Services from './components/services/Services'
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Cursor />
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+      {/* Framer Motion Crash Course */}
+      {/* <Test/>
+    <Test/> */}
+    </div>
+  );
 };
 
 export default App;
